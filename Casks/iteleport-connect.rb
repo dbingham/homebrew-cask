b3df2495b175fd31dd6336c2502fbab349afaf5f
Daniel Bingham <git@dbingham.com>
2014-08-13 16:17:28 -0700

    Add iTeleport Connect 6.0.0.2

---
+++ Casks/iteleport-connect.rb
@@ -0,0 +1,9 @@
+class IteleportConnect < Cask
+  version '6.0.0.2'
+  sha256 '2a845a052dfa04ae8a5c26da918944082f4a3a43ba3804cb8a078a77ad9f3b4c'
+
+  url 'http://www.iteleportmobile.com/download/iTeleport%20Installer.v6.0.0.2.dmg'
+  homepage 'http://www.iteleportmobile.com/connect'
+
+  link 'iTeleport Connect.app'
+end
